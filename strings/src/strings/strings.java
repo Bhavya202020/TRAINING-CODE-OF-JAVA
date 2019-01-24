f38e2c662a2bad8503a610b69ff127bcb71237a6
puneet <puneet@puneet-VAIO>
2019-01-24 21:03:59 +0530

    Happy Republic Day In Hindi Using Unicode!!

---
+++ strings/src/strings/strings.java
@@ -0,0 +1,17 @@
+package strings;
+
+public class strings {
+
+	public static void main(String[] args) {
+
+		String str = new String("Happy Republic Day");
+		System.out.println(" "+str);
+        System.out.println();
+		System.out.print("\u0939" + "\u0948" + "\u092A" + "\u092A" +"\u0940");
+		System.out.print(" " + " \u093F" + "\u0930" + "\u092A" + "\u093F" + "\u092C" + "\u0932" + "\u0915" );
+		System.out.print(" " +   " \u0947" + "\u0921");
+		
+		
+	}
+
+}
